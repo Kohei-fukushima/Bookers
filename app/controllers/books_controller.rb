bb382183739b52fc2e8caa309606bbd797373292
KOHEI FUKUSHIMA <pecpec1129@gmail.com>
2022-05-13 05:24:55 +0000

    [Start]rais-submi

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,48 @@
 class BooksController < ApplicationController
   def new
+   @book = Book.new
   end
 
+  def create
+    flash[:notice] = "Book was successfully created."
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+ def update
+   flash[:notice] = "Book was successfully created."
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book.id)
+ end
+ 
+ def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to book_path(book.id)
+ end
+
+ private
+ #ストロングパラメータ
+ def book_params
+   params.require(:book).permit(:title,:body)
+ end
+
+
 end
+
+
